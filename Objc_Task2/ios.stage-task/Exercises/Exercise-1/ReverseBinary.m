633988ed238085db419e0a9bf84a31810fe66f5b
Daria Astapova <>
2021-05-14 13:05:49 +0300

    feat: implement 3 tasks

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,34 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    NSMutableArray *binArray = [NSMutableArray new];
+    
+    while (n > 0) {
+        NSNumber *x = [NSNumber numberWithInt:n % 2];
+        [binArray addObject:x];
+        n = n / 2;
+    }
+    
+    while (binArray.count < 8) {
+        [binArray addObject: @0];
+    }
+    
+    NSArray *reversedArray = [[binArray reverseObjectEnumerator] allObjects];
+    NSMutableArray *sumArray = [NSMutableArray new];
+    int power = 1;
+   
+    for (NSUInteger i = 0; i < 8; i++) {
+        int item = [[reversedArray objectAtIndex:i] intValue];
+        int multiply = item * power;
+        [sumArray addObject: [NSNumber numberWithInt:multiply]];
+        power *= 2;
+    }
+    
+    int result = 0;
+    
+    for (NSNumber *i in sumArray) {
+        result += [i intValue];
+    }
+    
+    return result;
 }
--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,38 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    NSString *result = @"";
+    NSMutableString *string = [NSMutableString new];
+    NSMutableArray *array = [NSMutableArray new];
+    
+    if (numbersArray != nil && numbersArray.count > 0) {
+        for (NSNumber *i in numbersArray) {
+            if ([i intValue] > 0) {
+                if ([i intValue] < 255 && array.count <= 7) {
+                    [array addObject:[i stringValue]];
+                } else {
+                    result = @"The numbers in the input array can be in the range from 0 to 255.";
+                }
+            } else {
+                [array removeAllObjects];
+                result = @"Negative numbers are not valid for input.";
+            }
+        }
+        
+        if ([result isEqualToString:@""]) {
+            while (array.count < 4) {
+                [array addObject:@"0"];
+            }
+            
+            while (array.count > 4) {
+                [array removeLastObject];
+            }
+            
+            result = [array componentsJoinedByString:@"."];
+        }
+    }
+    
+    return result;
 }
 
 @end
